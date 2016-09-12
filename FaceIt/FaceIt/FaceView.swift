30deaa157b4ab0b8cbe9980556fd2907f34f9736
Devorah Langsam <devorah@langsam.org>
2016-09-12 13:30:20 -0400

    Make skull radius and center computed properties

--- FaceIt/FaceIt/FaceView.swift
+++ FaceIt/FaceIt/FaceView.swift
@@ -9,15 +9,18 @@
 import UIKit
 
 class FaceView: UIView {
-     
     
+    var skullRadius: CGFloat{
+        return min(bounds.size.width, bounds.size.height) / 2
+    }
+
+    var skullCenter: CGPoint{
+        return (x: bounds.midX, y: bounds.midY)
+    }
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
 
-        let skullRadius = min(bounds.size.width, bounds.size.height) / 2
-        //var skullCenter = convertPoint(center, fromView: superview)
-        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
         
         let skull = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0.0, endAngle: CGFloat(2*M_PI), clockwise: false)
         skull.lineWidth = 5.0
